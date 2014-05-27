e63d2ae517304244cc6874063f2c6f8cc5002cc8
gmp89 <gm.petrov@yahoo.fr>
2014-05-27 22:19:48 +0200

    test

--- includes/struct.h
+++ includes/struct.h
@@ -1,31 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lemipc.h                                           :+:      :+:    :+:   */
+/*   struct.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gpetrov <gpetrov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/05/26 19:20:20 by gpetrov           #+#    #+#             */
-/*   Updated: 2014/05/27 19:36:15 by gpetrov          ###   ########.fr       */
+/*   Created: 2014/05/27 14:51:19 by gpetrov           #+#    #+#             */
+/*   Updated: 2014/05/27 19:36:16 by gpetrov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LEMIPC_H
-# define LEMIPC_H
+#ifndef STRUCT_H
+# define STRUC_H
+# define TRUE	1
+# define FALSE	-1
+# include "lemipc.h"
 
-# define WIDTH	20
-# define HEIGHT	10
-# define KEY	424242
-
-# include "libft.h"
-# include "struct.h"
-
-/*
-**	Prototypes
-*/
-
-void	fill_map(t_share *shared);
-void	print_map(char **map);
-char	**create_map(void);
+typedef struct		s_share
+{
+	char			**map;
+	int				first;
+}					t_share;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,14 +6,95 @@
 /*   By: gpetrov <gpetrov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/26 19:21:18 by gpetrov           #+#    #+#             */
-/*   Updated: 2014/05/26 19:23:36 by gpetrov          ###   ########.fr       */
+/*   Updated: 2014/05/27 22:19:19 by gpetrov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <sys/stat.h>
 #include "lemipc.h"
 
-int		main(void)
+void	exit_error(char *msg)
 {
-	ft_putstr("lemipc\n");
+	printf("\033[31m%s\033[0m", msg);
+	exit(0);
+}
+
+char	**create_map(void)
+{
+	char	**map;
+	int		i;
+
+	i = 0;
+	map = (char **)malloc(sizeof(char *) * (HEIGHT + 1));
+	while (i < HEIGHT)
+	{
+		map[i] = (char *)malloc(sizeof(char) * (WIDTH + 1));
+		map[i][WIDTH] = 0;
+		i++;
+	}
+	map[HEIGHT] = NULL;
+	return (map);
+}
+
+void	print_map(char **map)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (map[i])
+	{
+		while (map[i][j] != 0)
+		{
+			if (map[i][j] == '.')
+			{
+				ft_putstr("\033[33m\u25CE\033[0m");
+				write(1, " ", 1);
+			}
+			write(1, " ", 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		i++;
+		j = 0;
+	}
+}
+
+void	fill_map(t_share *shared)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (i < HEIGHT)
+	{
+		while (j < WIDTH)
+		{
+			shared->map[i][j] = '.';
+			j++;
+		}
+		i++;
+		j = 0;
+	}
+
+}
+
+int		main(int ac, char **av)
+{
+//	char		cross = ❌;
+//	 printf("%d", cross);
+//	ft_putstr("\u05D4\n");
+//	ft_putstr("\u272C\n");
+//	ft_putstr("\033[32mlemipc \033[0m\n");
+
+	(void)av;
+	if (ac != 2)
+		exit_error("[USAGE] - ./lemipc <team>\n");
 	return (0);
 }
