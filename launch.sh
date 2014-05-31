02ab054459b4e23b90340d45747d744612ce85c2
gmp89 <gm.petrov@yahoo.fr>
2014-05-31 21:56:03 +0200

    update

--- launch.sh
+++ launch.sh
@@ -58,3 +58,36 @@ end tell'
 osascript -e 'tell app "Terminal"
 	do script "cd /Volumes/Data/nfs/zfs-student-2/users/2013/gpetrov/Rendu/S2/UNIX_2/Lem-IPC && ./lemipc B"
 end tell'
+osascript -e 'tell app "Terminal"
+	do script "cd /Volumes/Data/nfs/zfs-student-2/users/2013/gpetrov/Rendu/S2/UNIX_2/Lem-IPC && ./lemipc C"
+end tell'
+osascript -e 'tell app "Terminal"
+	do script "cd /Volumes/Data/nfs/zfs-student-2/users/2013/gpetrov/Rendu/S2/UNIX_2/Lem-IPC && ./lemipc C"
+end tell'
+osascript -e 'tell app "Terminal"
+	do script "cd /Volumes/Data/nfs/zfs-student-2/users/2013/gpetrov/Rendu/S2/UNIX_2/Lem-IPC && ./lemipc C"
+end tell'
+osascript -e 'tell app "Terminal"
+	do script "cd /Volumes/Data/nfs/zfs-student-2/users/2013/gpetrov/Rendu/S2/UNIX_2/Lem-IPC && ./lemipc C"
+end tell'
+osascript -e 'tell app "Terminal"
+	do script "cd /Volumes/Data/nfs/zfs-student-2/users/2013/gpetrov/Rendu/S2/UNIX_2/Lem-IPC && ./lemipc C"
+end tell'
+osascript -e 'tell app "Terminal"
+	do script "cd /Volumes/Data/nfs/zfs-student-2/users/2013/gpetrov/Rendu/S2/UNIX_2/Lem-IPC && ./lemipc C"
+end tell'
+osascript -e 'tell app "Terminal"
+	do script "cd /Volumes/Data/nfs/zfs-student-2/users/2013/gpetrov/Rendu/S2/UNIX_2/Lem-IPC && ./lemipc C"
+end tell'
+osascript -e 'tell app "Terminal"
+	do script "cd /Volumes/Data/nfs/zfs-student-2/users/2013/gpetrov/Rendu/S2/UNIX_2/Lem-IPC && ./lemipc C"
+end tell'
+osascript -e 'tell app "Terminal"
+	do script "cd /Volumes/Data/nfs/zfs-student-2/users/2013/gpetrov/Rendu/S2/UNIX_2/Lem-IPC && ./lemipc C"
+end tell'
+osascript -e 'tell app "Terminal"
+	do script "cd /Volumes/Data/nfs/zfs-student-2/users/2013/gpetrov/Rendu/S2/UNIX_2/Lem-IPC && ./lemipc C"
+end tell'
+osascript -e 'tell app "Terminal"
+	do script "cd /Volumes/Data/nfs/zfs-student-2/users/2013/gpetrov/Rendu/S2/UNIX_2/Lem-IPC && ./lemipc C"
+end tell'
